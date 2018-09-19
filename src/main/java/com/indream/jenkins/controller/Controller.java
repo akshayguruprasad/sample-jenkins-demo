e6a4ea99ef42a465f52c2b5d9f14e79a512fcc10
akshayguruprasad <akshayguruprasad@gmail.com>
2018-09-19 17:00:45 +0530

    auto deploy2

--- src/main/java/com/indream/jenkins/controller/Controller.java
+++ src/main/java/com/indream/jenkins/controller/Controller.java
@@ -1,5 +1,7 @@
 package com.indream.jenkins.controller;
 
+import java.net.HttpURLConnection;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +10,9 @@ public class Controller {
 
 	@GetMapping("/msg")
 	public String method() {
-		return "Welcome user, you are in Bridgelabz";
+		
+		
+		return "Welcome user, you are in Bridgelabz hello";
 		
 		
 	}
