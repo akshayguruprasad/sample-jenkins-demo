61f7b70efb6a3f35063231ea97496e29b692cfb6
akshayguruprasad <akshayguruprasad@gmail.com>
2018-09-19 15:32:31 +0530

    first

---
+++ src/main/java/com/indream/jenkins/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.indream.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping("/msg")
+	public String method() {
+		return "Welcome user";
+		
+		
+	}
+}
